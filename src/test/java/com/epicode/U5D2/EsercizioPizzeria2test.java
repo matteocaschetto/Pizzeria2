721ef858ebf9cf9968492d38e64d5b6549c2b8d2
matteocaschetto <matteocaschetto9@gmail.com>
2025-02-05 17:34:58 +0100

    Commit PIZZERIA2 _ 2

---
+++ src/test/java/com/epicode/U5D2/EsercizioPizzeria2test.java
@@ -0,0 +1,23 @@
+package com.epicode.U5D2;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+
+@SpringBootTest
+public class EsercizioPizzeria2test {
+
+
+
+    @Test
+    void contextLoads(){
+        assert true;
+    }
+
+
+
+
+
+}
